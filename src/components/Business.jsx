d822084615c018131c95ffbae90bc3db00aa603b
Noor_Mohammad <unknown2832021@gmail.com>
2022-09-06 16:34:41 +0600

    Testimonial added

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,15 +3,29 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 const FeatureCard = ({ icon, title, content, index }) => (
-  <div>
-    <div>
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}
+  >
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
       <img src={icon} alt="icons" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px]">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
     </div>
   </div>
 );
 
 const Business = () => (
-  <section id="features" className={`${layout.section} sm:px-4 md:px-0`}>
+  <section id="features" className={`${layout.section} px-4 md:px-0`}>
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
         You do the business, <br className="sm:block hidden" /> we'll handle the
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+const CardDeal = () => (
+  <section className={`${layout.section} px-4 md:px-0`}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few eay
+        steps
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam,
+        voluptate minus fugiat laboriosam perferendis eveniet neque esse. Soluta
+        saepe, recusandae architecto exercitationem labore earum non?
+      </p>
+      <Button styles="mt-10" />
+    </div>
 
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
 export default CardDeal;
